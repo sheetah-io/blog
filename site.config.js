5c9975963d7d31a9399c28599e3b48784009a4a3
Jihun Ko <37041154+lopun@users.noreply.github.com>
2023-10-03 17:21:27 +0900

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,25 +1,25 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "morethanmin",
+    name: "Sheetah",
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
-    role: "frontend developer",
+    role: "CEO",
     bio: "I develop everything using node.",
-    email: "morethanmin.dev@gmail.com",
-    linkedin: "morethanmin",
-    github: "morethanmin",
+    email: "contact@sheetah.io",
+    linkedin: "sheetah-io",
+    github: "sheetah-io",
     instagram: "",
   },
   projects: [
     {
-      name: `morethan-log`,
-      href: "https://github.com/morethanmin/morethan-log",
+      name: `Sheetah`,
+      href: "https://sheetah.io",
     },
   ],
   // blog setting (required)
   blog: {
-    title: "morethan-log",
-    description: "welcome to morethan-log!",
+    title: "Sheetah Blog",
+    description: "Welcome to Sheetah Blog!",
   },
 
   // CONFIG configration (required)
@@ -47,9 +47,9 @@ const CONFIG = {
     },
   },
   utterances: {
-    enable: true,
+    enable: false,
     config: {
-      repo: process.env.NEXT_PUBLIC_UTTERANCES_REPO || "",
+      repo: "sheetah-io/blog",
       "issue-term": "og:title",
       label: "💬 Utterances",
     },
